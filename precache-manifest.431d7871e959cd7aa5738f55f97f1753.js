67420c0881de0e95225aa5b61b2383bbdc487166
SK851831 <sk851831@gmail.com>
2019-12-03 02:58:18 +0530

    Updates

---
+++ precache-manifest.431d7871e959cd7aa5738f55f97f1753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f667fb435e1a38e23a16f8beeef37d96",
+    "url": "/react-learn/index.html"
+  },
+  {
+    "revision": "61831b24b62fb3f40dc4",
+    "url": "/react-learn/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f87b9ee3988bbbcec51c",
+    "url": "/react-learn/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "61831b24b62fb3f40dc4",
+    "url": "/react-learn/static/js/2.45b44d3d.chunk.js"
+  },
+  {
+    "revision": "f87b9ee3988bbbcec51c",
+    "url": "/react-learn/static/js/main.24e4e68d.chunk.js"
+  },
+  {
+    "revision": "0d9d6dee5bef53fd1c9b",
+    "url": "/react-learn/static/js/runtime-main.138b867a.js"
+  }
+]);
